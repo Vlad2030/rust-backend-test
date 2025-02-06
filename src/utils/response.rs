958a20286436996038ccc9ce1349c2bf296663c3
vladops <vlad123123v@gmail.com>
2025-02-06 14:01:21 +0300

    Update `utils`

--- src/utils/response.rs
+++ src/utils/response.rs
@@ -1,11 +1,11 @@
-pub fn ok_response_json<T>(data: &T) -> ntex::web::HttpResponse
+pub fn ok_json<T>(data: &T) -> ntex::web::HttpResponse
 where
     for<'de> T: serde::Serialize + serde::Deserialize<'de>,
 {
     ntex::web::HttpResponse::Ok().json(data)
 }
 
-pub fn created_response_json<T>(data: &T) -> ntex::web::HttpResponse
+pub fn created_json<T>(data: &T) -> ntex::web::HttpResponse
 where
     for<'de> T: serde::Serialize + serde::Deserialize<'de>,
 {
