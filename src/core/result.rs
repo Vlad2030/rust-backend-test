88eaad06872d527ca9a274f6d7a908f5b7e7714b
vladops <vlad123123v@gmail.com>
2025-02-06 14:02:42 +0300

    Update `result`

--- src/core/result.rs
+++ src/core/result.rs
@@ -4,5 +4,3 @@ pub type Result<T> = std::result::Result<T, core::error::Error>;
 pub type ApiResult = Result<ntex::web::HttpResponse>;
 pub type QueryResult<T> =
     std::result::Result<ntex::web::types::Query<T>, ntex::web::error::QueryPayloadError>;
-pub type PathResult<T> =
-    std::result::Result<ntex::web::types::Path<T>, ntex::web::error::PathError>;
